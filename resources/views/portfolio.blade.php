345b3df689e9a8d116a93459b75779a76155b4a4
Asep <excelparinussa23@gmail.com>
2024-11-23 21:09:59 +0800

    Portfolio

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Portfolio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/resume.min.css" rel="stylesheet">
+
+    <!--Icon Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top" style="margin-left: 10px;">
+        <span class="d-block d-lg-none">Welcome</span>
+        <span class="d-none d-lg-block" style="margin-left: 16px; ">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.webp" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 10px;">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards & Certifications</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#project">Project</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Excel Marcello
+            <span class="text-primary">Parinussa</span>
+          </h1>
+          <div class="subheading mb-5">Student · Network Security and Digital Forensic Enthusiast ·
+            <a href="mailto:excelparinussa24@gmail.com">excelparinussa24@gmail.com</a>
+          </div>
+          <p class="mb-5">Hello! I’m a cybersecurity enthusiast with a strong focus on networking and digital forensics. I’m passionate about solving CTF challenges, unraveling security vulnerabilities, and enhancing system defenses.<p>In addition to my expertise in cybersecurity, I have hands-on experience in backend development, particularly with frameworks like Spring Boot and Laravel. My portfolio highlights my journey, projects, and achievements in these areas.</p></p>
+          <ul class="list-inline list-social-icons" style="margin-top: 40px;">
+            <a type="button" class="btn btn-lg disable" href="https://instagram.com/prnssaexcel" target="_blank" style="background-color: #9e0505; color: white;">
+              <i class="bi bi-instagram"></i>
+            </a>
+            <a type="button" class="btn btn-lg disable" href="https://github.com/veryepiccindeed" target="_blank" style="background-color: #9e0505; color: white; margin-left: 10px;">
+              <i class="bi bi-github"></i>
+            </a>
+            <a type="button" class="btn btn-lg disable" href="https://steamcommunity.com/id/feder4le" target="_blank" style="background-color: #9e0505; color: white;  margin-left: 10px;">
+              <i class="bi bi-steam"></i>
+            </a>
+          </ul>
+        </div>       
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+            <h2 class="mb-5">Experience</h2>
+    
+            <!-- Experience Item 1 -->
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content me-auto">
+                    <h3 class="mb-0">GPIB Immanuel Makassar Multimedia</h3>
+                    <div class="subheading mb-3">OBS Operator</div>
+                </div>
+                <div class="resume-date text-md-end">
+                    <span class="text-primary">April 2023 - Present</span>
+                </div>
+            </div>
+    
+            <!-- Experience Item 2 -->
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content me-auto">
+                    <h3 class="mb-0">SMK Telkom Makassar E-Sports</h3>
+                    <div class="subheading mb-3">Patturukang Ji</div>
+                </div>
+                <div class="resume-date text-md-end">
+                    <span class="text-primary">December 2021 - March 2022</span>
+                </div>
+            </div>
+    
+            <!-- Experience Item 3 -->
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content me-auto">
+                    <h3 class="mb-0">Informatics Student Union UC Makassar</h3>
+                    <div class="subheading mb-3">Patturukang Ji Juga</div>
+                </div>
+                <div class="resume-date text-md-end">
+                    <span class="text-primary">September 2023 - July 2024</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content me-auto">
+              <h3 class="mb-0">Ciputra University Makassar</h3>
+              <div class="subheading mb-3">Currently Majoring In Informatics (Computer Science)</div>
+            </div>
+            <div class="resume-date text-md-end">
+              <span class="text-primary">September 2023 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content me-auto">
+              <h3 class="mb-0">Telkom Vocational School Makassar</h3>
+              <div class="subheading mb-3">Computer and Network Engineering Graduate</div>
+            </div>
+            <div class="resume-date text-md-end">
+              <span class="text-primary">July 2020 - May 2023</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Knowledges &amp; Tools</div>
+          <ul class="list-inline list-icons">
+          </ul>
+
+          <div class="subheading mb-3"></div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Basic Network</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Basic Routing</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Basic Network Security</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Basic Digital Forensic</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Spring Boot</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Laravel</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Canva</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Figma</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Microsoft Word</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Microsoft PowerPoint</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <p>I’m a big fan of movies, especially the kind that keep me guessing until the end—or at least until I Google the plot halfway through. Gaming is my go-to for sharpening my problem-solving skills (and my ability to lose gracefully). And music? It’s my daily dose of relaxation and the perfect soundtrack for pretending I have my life together.</p>
+          <p>On the outdoor front, I'm sometimes a socialite, delighting in the art of meaningful (and occasionally not important) conversations with friends. Because, you know, life's too short not to procrastinate and share a good laugh!</p>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Awards &amp; Certifications</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Finalist - National Pateron Olympiad Vol. 2 (English Language Division) 2022 - High School/Equivalent Level</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Gold Medalist -  Puskanas Indonesian Science Competition 2022 (English Language Division) 2022 - High School/Equivalent Level</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              3<sup>rd</sup>
+              Place Achiever - Dalton Science Olympiad (English Language Division) - High School/Equivalent Level</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Participant of National Cyber Week 2024 CTF, Held by Binus University</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Participant of COMPFEST 16 2024 CTF, Held by University of Indonesia</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="project">
+      <div class="my-auto">
+        <h2 class="mb-5">Project</h2>
+        <div class="container">
+          <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+              <div class="card hover-expand">
+                <img src="assets/img/pulauwesi.webp" class="card-img-top img-fixed" alt="Card 1" loading="lazy">
+                <div class="card-body">
+                  <h5 class="card-title">Pulauwesi</h5>
+                  <p class="card-text" style="color: #868e96;">Gateway to discovering and exploring the diverse islands of Sulawesi</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card hover-expand">
+                <img src="assets/img/others.ts.webp" class="card-img-top img-fixed" alt="Card 2" loading="lazy">
+                <div class="card-body">
+                  <h5 class="card-title">Others.ts</h5>
+                  <p class="card-text" style="color: #868e96;">A platform to brings unique stories from diverse voices within the SMK Telkom Makassar community</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card hover-expand">
+                <img src="assets/img/questlist.webp" class="card-img-top img-fixed" alt="Card 3" loading="lazy">
+                <div class="card-body">
+                  <h5 class="card-title">QuestList</h5>
+                  <p class="card-text" style="color: #868e96;">Turn everyday tasks into exciting challenges and earn real-world rewards</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+  
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="assets/js/resume.min.js" defer></script>
+
+  </body>
+
+</html>
